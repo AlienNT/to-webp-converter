f1fde1c5ecca6ba77f6f8e07c0e266bb2a336629
Trysten M <nick.tristen22@gmail.com>
2024-03-13 20:02:02 +0200

    update(api): update index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+import Express from 'express'
+import dotenv from 'dotenv'
+
+import hostConfig from "./src/configs/hostConfig.js";
+import apiConfig from "./src/configs/apiConfig.js";
+import routes from "./src/routes/index.js";
+import bodyParser from "body-parser";
+
+import cors from "cors";
+
+dotenv.config()
+
+const APP = new Express()
+    .use(cors())
+    .use(apiConfig.API_ROUTE, routes)
+    // .use(bodyParser.urlencoded({extended: true}))
+    .use(bodyParser.json({limit: '20mb'}))
+
+async function start() {
+    try {
+        APP.listen(hostConfig.PORT, () => {
+            console.log('server started in port: ', hostConfig.PORT)
+        })
+    } catch (e) {
+        console.log('server start error', e)
+    }
+}
+
+await start()

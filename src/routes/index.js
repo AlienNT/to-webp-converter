3e52ab59d0008854af515e592a570f5ef421fc33
Trysten M <nick.tristen22@gmail.com>
2024-03-13 20:02:40 +0200

    update(api/routes): add image controller to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import {Router} from "express";
+import multer from "multer";
+
+import ImageController from "../controllers/imageController.js";
+
+const upload = multer()
 
 const router = new Router()
-    .post('/single')
-    .post('/multiple')
-
+    .post('/convert-single', upload.single('file'), ImageController.convertSingle)
+    .post('/convert-multiple', upload.single('files'), ImageController.convertMultiple)
 export default router

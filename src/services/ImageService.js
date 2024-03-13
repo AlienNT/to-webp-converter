6b7ec4e146779fdc93b4d187efd7e6c1b6ee7d12
Trysten M <nick.tristen22@gmail.com>
2024-03-13 20:03:26 +0200

    add(api/services): add image service

---
+++ src/services/ImageService.js
@@ -0,0 +1,47 @@
+import sharp from "sharp";
+
+class ImageService {
+    async convertToWebp({image, name}) {
+        const convertedImageBuffer = await sharp(image).webp().toBuffer()
+
+        const {
+            size,
+            width,
+            height,
+            format
+        } = await this.getMeta(convertedImageBuffer)
+
+        const mimetype = 'image/' + format
+        const src = this.bufferToBase64(convertedImageBuffer, mimetype)
+
+        return {
+            src,
+            size,
+            width,
+            height,
+            format,
+            mimetype,
+            name: this.createImageName(name, format)
+        };
+    }
+
+    bufferToBase64(buffer, mimetype) {
+        return `data:${mimetype};base64,${Buffer.from(buffer, 'binary').toString('base64')}`
+    }
+
+    createImageName(name, format) {
+        if (!format) return name
+
+        return this.getName(name) + (format[0] === '.' ? `${format}` : `.${format}`)
+    }
+
+    getName(name) {
+        return name?.split('.')?.[0] || name
+    }
+
+    async getMeta(buffer) {
+        return await sharp(buffer).metadata()
+    }
+}
+
+export default new ImageService()

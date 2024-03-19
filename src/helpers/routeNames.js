266bfcb3504d97b5612b0298e427e092f63cf18b
Trysten M <nick.tristen22@gmail.com>
2024-03-19 20:14:33 +0200

    fix(api/helpers): fix routes format

--- src/helpers/routeNames.js
+++ src/helpers/routeNames.js
@@ -1,5 +1,5 @@
 export const routeNames = {
-    CONVERT_SINGLE: 'convert-single',
-    CONVERT_MULTIPLE: 'convert-multiple',
-    IS_ALIVE: 'is-alive',
+    CONVERT_SINGLE: '/convert-single',
+    CONVERT_MULTIPLE: '/convert-multiple',
+    IS_ALIVE: '/is-alive',
 }
